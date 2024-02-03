1fb7db8e321342730102b11d9821299a50640995
Californian <1079539+Californian@users.noreply.github.com>
2024-02-03 00:49:07 -0800

    Fix: page title is too small.
    This reverts commit dc7ba29f540125634b23b302443e7e0e74b20bf3.

--- src/components/AppTitle/AppTitle.tsx
+++ src/components/AppTitle/AppTitle.tsx
@@ -11,7 +11,7 @@ const AppTitle: React.FC<AppTitleProps> = ({ title }) => {
 
   return (
     <Center>
-      <Title order={5}>{appTitle}</Title>
+      <Title>{appTitle}</Title>
     </Center>
   );
 };
